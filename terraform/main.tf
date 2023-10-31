b3d521335a10cf1051f427bd1c36015aaea40187
Aransiola Tolulope <TolulopeTheophilus.Aransiola@kpmg.co.uk>
2023-10-31 16:17:12 +0100

    dependabot

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,3 +8,11 @@ resource "azurerm_resource_group" "rg" {
   location = "uksouth"
 
 }
+
+module "vnet" {
+  source  = "Azure/vnet/azurerm"
+  version = "3.1.0"
+ 
+  resource_group_name = azurerm_resource_group.rg.name
+  vnet_location = "uksouth"
+}
